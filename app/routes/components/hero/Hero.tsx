ec6807d9c01a83fbd7922837259db321528a4f9c
Matias Alvarez <matiasalvarezbustos@gmail.com>
2025-09-25 16:27:18 -0300

    feat: modificacion hero

--- app/routes/components/hero/Hero.tsx
+++ app/routes/components/hero/Hero.tsx
@@ -4,20 +4,28 @@ import Button from '../utils/button/Button';
 
 export default function Hero() {
     return (
-        <div className={styles.hero}>
-            <h1 className={styles.title}>
-                DESARROLLO <br/> WEB
-            </h1>
-            <span className={styles.subtitle}></span>
-            <h2>
-                Servicios de desarrollo web enfocados en la creación de sitios modernos, funcionales y adaptados a las
-                necesidades de cada proyecto. Se prioriza el diseño atractivo, la optimización del rendimiento y la
-                implementación de buenas prácticas de programación, con el objetivo de impulsar la presencia digital
-                de negocios y emprendimientos, fortaleciendo su identidad en línea y generando resultados sostenibles
-                en el tiempo.
-            </h2>
+        <>
+            <div className={styles.container__hero}>
+                <div className={styles.container__img}>
+                    <img src="/perfil1.png" alt="" />
+                </div>
+                <div className={styles.hero}>
 
-            <Button txtButton='Contáctame' />
-        </div>
+                    <h1 className={styles.title}>
+                        DESARROLLO <br /> WEB
+                    </h1>
+                    <span className={styles.subtitle}></span>
+                    <h2>
+                        Servicios de desarrollo web enfocados en la creación de sitios modernos, funcionales y adaptados a las
+                        necesidades de cada proyecto. Se prioriza el diseño atractivo, la optimización del rendimiento y la
+                        implementación de buenas prácticas de programación, con el objetivo de impulsar la presencia digital
+                        de negocios y emprendimientos, fortaleciendo su identidad en línea y generando resultados sostenibles
+                        en el tiempo.
+                    </h2>
+
+                    <Button txtButton='Contáctame' />
+                </div>
+            </div>
+        </>
     );
 };
